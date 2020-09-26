796bcdfd91697a1d3065939aee2e7ebc2e665bac
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-09-26 16:51:07 +0530

    hey

--- File2.java
+++ File2.java
@@ -1,6 +1,8 @@
 public class File2{
 public static void main(String[] args){
-int PLAYER_POSITION=0,option=0;
+int PLAYER_POSITION=0,option=0,count=0;
+while(PLAYER_POSITION<100){
+count++;
 do{
 int rollDice=(int)(Math.random()*6)+1;
 option=(int)(Math.random()*3);
@@ -18,7 +20,9 @@ System.out.println("No Play"+PLAYER_POSITION);
 }
 if(PLAYER_POSITION<0)
 PLAYER_POSITION=0;
+System.out.println("Current position: "+PLAYER_POSITION);
 }
-while(PLAYER_POSITION==100);
+while(PLAYER_POSITION!=100 && option==1);
+}
 }
 }
