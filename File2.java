5c25fdb9b3c8fd63ee9959632d1d2b80a621da97
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-09-26 16:15:57 +0530

    hey

--- File2.java
+++ File2.java
@@ -1,6 +1,19 @@
 public class File2{
 public static void main(String[] args){
 int rollDice=(int)(Math.random()*6)+1;
-System.out.println("Dice roll value:"+rollDice);
+int PLAYER_POSITION=0,option=0;
+option=(int)(Math.random()*3);
+switch(option){
+case 1:
+PLAYER_POSITION+=rollDice;
+System.out.println("Ladder"+PLAYER_POSITION);
+break;
+case 2:
+PLAYER_POSITION-=rollDice;
+System.out.println("Snake"+PLAYER_POSITION);
+break;
+default:
+System.out.println("No Play"+PLAYER_POSITION);
+}
 }
 }
